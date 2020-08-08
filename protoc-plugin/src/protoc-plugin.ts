29b2933762f090be7d4eb2187707ecd846bab42c
Warren Falk <warren@warrenfalk.com>
2020-08-08 16:54:27 -0400

    STASH generate our own descriptor in protoc-plugin

--- protoc-plugin/src/protoc-plugin.ts
+++ protoc-plugin/src/protoc-plugin.ts
@@ -1,4 +1,5 @@
 export * from "../gen/google/protobuf/compiler/plugin_pb";
-export * from "google-protobuf/google/protobuf/descriptor_pb";
+export * from "../gen/google/protobuf/descriptor_pb";
+//export * from "google-protobuf/google/protobuf/descriptor_pb";
 export * from "google-protobuf";
 
