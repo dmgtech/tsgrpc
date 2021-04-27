99b9a30503ec53e7d200f38d4db505c6f509eea5
Warren Falk <warren@warrenfalk.com>
2021-04-27 13:11:13 -0400

    add testserver

---
+++ test/testserver/TestServerMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Tsgrpc.Testserver;
+
+namespace testserver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = System.Environment.GetEnvironmentVariable("HOST") ?? "localhost";
+            var port = int.Parse(System.Environment.GetEnvironmentVariable("PORT") ?? "50051");
+
+            var tcs = new TaskCompletionSource();
+            Console.CancelKeyPress += (sender, o) => {
+                tcs.SetResult();
+            };
+
+            Server server = new Server
+            {
+                Services = { FooApi.BindService(new FooImpl()) },
+                Ports = { new ServerPort(host, port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine($"Foo API server listening on {host}:{port}");
+            Console.WriteLine("Press Ctrl+C stop the server...");
+
+            await tcs.Task;
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    class FooImpl : FooApi.FooApiBase {
+        public override async Task GetTime(TimeRequest request, IServerStreamWriter<TimeUpdateReply> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"Request: GetTime(interval = {request.IntervalSeconds})");
+            var interval = request.IntervalSeconds == default ? 60 : request.IntervalSeconds;
+            for (;;) {
+                var timeString = DateTimeOffset.Now.ToString();
+                await responseStream.WriteAsync(new TimeUpdateReply {
+                    CurrentTimeString = timeString,
+                });
+                Console.WriteLine($"Sending time update: {timeString}");
+                await Task.Delay(interval);
+            }
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Request: SayHello ({request.Name})");
+            var message = $"Hello, {request}! -from FooApi";
+            Console.WriteLine($"Responding: {message}");
+            return Task.FromResult(new HelloReply {
+                Message = message,
+            });
+        }
+    }
+}
