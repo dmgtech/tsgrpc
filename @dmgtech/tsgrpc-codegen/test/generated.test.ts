b7539c41b7bdf4be9e2ad6f88d1397fd841906f5
Warren Falk <warren@warrenfalk.com>
2021-08-08 20:43:18 -0400

    add test to confirm that babel can compile reference with typescript preset

--- @dmgtech/tsgrpc-codegen/test/generated.test.ts
+++ @dmgtech/tsgrpc-codegen/test/generated.test.ts
@@ -1,4 +1,5 @@
 import ts from 'typescript'
+import {transformFileAsync, TransformOptions} from '@babel/core'
 import fs from 'fs'
 import path from 'path'
 
@@ -18,5 +19,14 @@ describe("compilability of generated code", () => {
         const diags = ts.getPreEmitDiagnostics(program);
         const errors = diags.filter(d => d.category === 1 /* Error */).map(d => d.messageText);
         expect(errors).toEqual([]);
+    })
+
+    it('can be compiled by babel', async () => {
+        const options: TransformOptions = {
+            presets: ['@babel/preset-typescript']
+        }
+        const output1 = await transformFileAsync(examplePath, options);
+        const output2 = await transformFileAsync(importPath, options);
+        console.log(output1);
     })
 })
